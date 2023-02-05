3965edaf0afa2fec3ec5cb631f8ad9dd93867c91
NaerDD <806915230@qq.com>
2023-02-05 22:33:36 +0800

    general-backstage first commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+// import { Button,Row,Container} from 'element-ui';
+//全局注入
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App.vue';
+import router from './router';
 
-Vue.config.productionTip = false
+//按需引入
+// Vue.use(Button)
+// Vue.use(Row)
+// Vue.use(Container)
+
+Vue.use(ElementUI);
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: "#app",
+  router,
+  render: (h) => h(App),
+});
